2e34f5ef51ea1923315232e8a13107dbfc392cfb
nbaak <niklas.baak@outlook.com>
2023-12-29 17:25:03 +0100

    fixed the bug where christmas is one day off

--- src/lib/christmas_countdown.py
+++ src/lib/christmas_countdown.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
+
 class ChristmasCountdown:
 
     @staticmethod
     def calculate_days_remaining():
-        current_date = datetime.now()
+        current_date = datetime(datetime.today().year, datetime.today().month, datetime.today().day)
         christmas_date = datetime(current_date.year, 12, 25)
 
         # Check if Christmas already passed this year, if yes, calculate for the next year
@@ -14,3 +15,13 @@ class ChristmasCountdown:
         # Calculate the remaining days until Christmas
         remaining_time = christmas_date - current_date
         return remaining_time.days
+
+
+def test():
+    days_remaining = ChristmasCountdown.calculate_days_remaining()
+
+    print(days_remaining)
+
+
+if __name__ == "__main__":
+    test()
