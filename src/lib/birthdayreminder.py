d5fec2817af6cd470ee25cbb7909cc82549647c1
nbaak <niklas.baak@outlook.com>
2024-01-02 15:28:38 +0100

    handling leap years in reminder

--- src/lib/birthdayreminder.py
+++ src/lib/birthdayreminder.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 from lib.admin_tools import load as fload
 from lib.admin_tools import save as fsave
+from lib.calendar import Calendar
 
 
 class BirthdayReminder():
@@ -12,16 +13,16 @@ class BirthdayReminder():
         print('Birthdays:', self.birthdays)
 
     def add_birthday(self, guild, user, year, month, day, show=False):
+        if Calendar.is_valid(int(day), int(month), int(year)):
+            if guild not in self.birthdays:
+                self.birthdays[guild] = {}
 
-        if guild not in self.birthdays:
-            self.birthdays[guild] = {}
+            # year = year if not show else None
+            bday_obj = {'day': f'{month}-{day}',
+                        'year': year if show else None,
+                        'show': show}
 
-        # year = year if not show else None
-        bday_obj = {'day': f'{month}-{day}',
-                    'year': year if show else None,
-                    'show': show}
-        
-        self.birthdays[guild][user] = bday_obj
+            self.birthdays[guild][user] = bday_obj
 
     def remove_birthday(self, guild, user):
         try:
@@ -29,19 +30,22 @@ class BirthdayReminder():
         except Exception as e:
             print(e)
 
-    def check_day(self, cdate=None):
-        current_date = datetime.now().strftime("%m-%d")
+    def check_day(self, guild_id, year:int=None, month:int=None, day:int=None):
+        cyear = year or datetime.today().year
+        cmonth = month or datetime.today().month
+        cday = day or datetime.today().month
 
-        cdate = cdate or current_date
+        cdate = f'{cmonth}-{cday}'
 
         birthdays_today = []
 
-        for gid, guild in self.birthdays.items():
+        if cdate == '3-1' and not Calendar.is_leap_year(cyear):
+            birthdays_today = self.check_day(guild_id, cyear, 2, 29)
 
-            for user, date in guild.items():
+        for user, date in self.birthdays[guild_id].items():
 
-                if date['day'] == cdate:
-                    birthdays_today.append((gid, user))
+            if date['day'] == cdate:
+                birthdays_today.append((user, date))
 
         return birthdays_today
 
@@ -61,28 +65,30 @@ class BirthdayReminder():
 
 def test():
     br = BirthdayReminder('test')
-    br.add_birthday('000', 'Test1' , '2023', '02', '01', False)
-    br.add_birthday('000', 'Test2' , '2023', '02', '01', False)
-    br.add_birthday('000', 'Test3' , '2023', '02', '01', False)
-    br.add_birthday('000', 'Test4' , '2023', '12', '31', False)
-    br.add_birthday('001', 'Test5' , '2023', '12', '31', False)
+    br.add_birthday('000', 'Test1' , 2004, 2, 29, False)
+    br.add_birthday('000', 'Test2' , 2005, 2, 29, False)  # invalid date
+    br.add_birthday('000', 'Test3' , 2023, 3, 1, False)
+    br.add_birthday('000', 'Test4' , 2023, 12, 31, False)
+    br.add_birthday('001', 'Test5' , 2023, 12, 31, False)
 
-    br.add_birthday('000', 'Test23', '2024', '01', '01', False)
-    br.add_birthday('000', 'Test24', '2024', '01', '01', False)
-    br.add_birthday('001', 'Test25', '2024', '01', '01', False)
+    br.add_birthday('000', 'Test23', 2024, 1, 1, False)
+    br.add_birthday('000', 'Test24', 2024, 1, 1, False)
+    br.add_birthday('001', 'Test25', 2024, 1, 1, False)
 
     print(br.birthdays)
-    print()
+    print(Calendar.is_leap_year(2000))
     print()
 
-    print('check:', br.check_day('02-01'))
+    print('G 000: check 3-1:', br.check_day('000', year=2023, month=3, day=1))
+    print('G 000: check 2-29:', br.check_day('000', year=2024, month=2, day=29))
+    print('G 000: check 3-1:', br.check_day('000', year=2024, month=3, day=1))
 
-    print(br.check_day('01-01'))
+    # print(br.check_day('01-01'))
     print(br)
     br.remove_birthday('000', 'Test1')
     br.remove_birthday('000', 'Test1')
     print(br)
-    print(br.check_day('01-01'))
+    # print(br.check_day('01-01'))
 
 
 if __name__ == "__main__":
