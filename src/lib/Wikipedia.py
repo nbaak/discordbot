5e94d3044f355ca4e04cba332aa383f81373f863
nbaak <niklas.baak@outlook.com>
2024-01-04 10:59:08 +0100

    formatting

--- src/lib/Wikipedia.py
+++ src/lib/Wikipedia.py
@@ -4,22 +4,22 @@ import random
 
 
 class WikipediaWrapper:
-    
+
     @staticmethod
     def search(topic):
         try:
             wikis = wikipedia.search(topic)
-            
+
             num_wikis = len(wikis)
-            
+
             # get ONE of them
             wiki = random.choice(wikis)
             summary = wikipedia.summary(wiki, sentences=3)
             page = wikipedia.page(wiki)
-            
+
             content = f"{page.title}\n{page.summary.strip()}\n\n{page.url}"
+            
         except Exception as e:
             content = "something went wrong :("
-        
-        
+
         return content
