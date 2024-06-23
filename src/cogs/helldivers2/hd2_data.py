883079f24f6d1a470f7e92f8927fd3fbc535f29b
nbaak <niklas.baak@outlook.com>
2024-06-23 12:14:46 +0200

    better detection of who owns what planet.. (the Mort case)

--- src/cogs/helldivers2/hd2_data.py
+++ src/cogs/helldivers2/hd2_data.py
@@ -1,7 +1,5 @@
 from cogs.helldivers2 import api
-import pickle
 import datetime
-import time
 from typing import Union
 
 
@@ -42,19 +40,20 @@ class HD2DataService():
         self.update_war_status()
         # self.update_war_info()
         
-    def find_in_campain_dict(self, search_key, search_value:str) -> dict:        
+    def find_in_campain_dict(self, search_key, search_value:str) -> Union[dict, None]: 
         for planet in self.campaign:
             for k, v in planet.items():
                 if k == search_key and v == search_value:
                     return planet        
         return None
     
-    def get_faction_for_planet(self, planet_id:int) -> str:
+    def get_faction_for_planet(self, planet_id:int) -> tuple[str, str]:
         owner = self.war_status["planetStatus"][planet_id]["owner"]
         factions = {1: "🌎", 2: "🪲", 3: "🤖"}
-        return factions[owner] if owner in factions else "?"
+        icon = factions[owner] if owner in factions else "?"
+        return owner, icon 
     
-    def faction_icon(self, faction:Union[int, str]):
+    def faction_icon(self, faction:Union[int, str]) -> str:
         if type(faction) == int:
             factions = {1: "🌎", 2: "🪲", 3: "🤖", 4: "🦑"}
         else:
@@ -63,7 +62,7 @@ class HD2DataService():
         
         return factions[faction] if faction in factions else "?"
     
-    def mo_progress(self, major_order:dict):
+    def mo_progress(self, major_order:dict) -> str:
         progress = major_order['progress']
         tasks = major_order['setting']['tasks']
         planets = [self.planets[str(task['values'][2])]['name'] for task in tasks]
@@ -73,30 +72,34 @@ class HD2DataService():
         for planet_name, prog, planet_id in list(zip(planets, progress, planet_ids)):
             
             planet = self.find_in_campain_dict('planetIndex', planet_id)
-            progress = planet['percentage'] if planet else prog*100
-            try:
-                # inside campaign
+            
+            if planet:
+                # part of campaign (attack of defense)
+                progress = planet['percentage']
                 defense = "🛡️" if planet['defense'] else "⚔️"
-                holder = self.faction_icon(planet['faction'])
-            except Exception as e:
-                # not inside campaign (reachable via supply lines)
-                defense = ""        
-                holder = self.get_faction_for_planet(planet_id)
+                holder_icon = self.faction_icon(planet['faction'])
+            else:
+                # not part of campaign
+                current_owner, holder_icon = self.get_faction_for_planet(planet_id)
+                progress = 100 if current_owner == 1 else prog * 100
+                defense = ""            
+            
+            # print(planet_name, planet, prog)                
                 
-            text += f"{holder}{defense} {planet_name}: {progress:3.2f}%\n"
+            text += f"{holder_icon}{defense} {planet_name}: {abs(progress):3.2f}%\n"
         
         return text
     
     def mo_time_remaining(self, major_order:dict=None) -> str:
         major_order = major_order or self.major_order[0]
-        if major_order:        
+        if major_order: 
             remaining = int(major_order['expiresIn'])
             delta = datetime.timedelta(seconds=remaining)
             return delta
         else:
             return '---'
         
-    def get_major_order(self):
+    def get_major_order(self) -> str:
         if self.major_order:
             mo = self.major_order[0]
             expires = int(self.major_order[0]['expiresIn'])
@@ -111,7 +114,7 @@ class HD2DataService():
         else:
             return f"MAJOR ORDER\nNo Major Order active!"
         
-    def get_campaign(self):
+    def get_campaign(self) -> str:
         if self.campaign:
             text = "War Campaign:\n"
             
