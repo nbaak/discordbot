e6dc24b3b018ad64b26c943a199a2db9ef50c93c
nbaak <niklas@baak.xyz>
2024-11-12 13:21:49 +0100

    new config parts

--- src/CONFIG.py
+++ src/CONFIG.py
@@ -1,6 +1,13 @@
 # Configuration
+
+# Login
 API_TOKEN = ''
+
+# Commands
 PREFIX = '!'
 
 # AI
 AI_CHANNELS = ['development']  # All Channels, if list is empty
+
+# Helldiver2
+X_SUPER_CONTACT = "" # insert your contact email here
