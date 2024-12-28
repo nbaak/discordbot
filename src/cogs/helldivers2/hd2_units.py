eb563cd86700a40e2e8d30c364ae038b8a66c735
nbaak <niklas@baak.xyz>
2024-12-28 16:41:04 +0100

    added illuminate units viotless and harvester

--- src/cogs/helldivers2/hd2_units.py
+++ src/cogs/helldivers2/hd2_units.py
@@ -6,7 +6,9 @@ enemy_units = {
     0: 'any', 
     2514244534: 'Bile Titan',
     1379865898: 'Bile Spewer',
-    2058088313: 'Warrior'
+    2058088313: 'Warrior',
+    4211847317: 'Voteles',
+    1405979473: 'Harvester'
 }
 
 def get_enemy(id:int) -> str:
