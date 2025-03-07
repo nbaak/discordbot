d4fb63cb6701802aa6793f77b337ea5a4023bdb3
nbaak <niklas@baak.xyz>
2025-03-07 17:09:15 +0100

    added ubit shredder tank

--- src/cogs/helldivers2/hd2_units.py
+++ src/cogs/helldivers2/hd2_units.py
@@ -4,15 +4,17 @@ a list of known enemies by id
 
 enemy_units = {
     0: 'any', 
-    2514244534: 'Bile Titan',
     1379865898: 'Bile Spewer',
+    1405979473: 'Harvester',
     2058088313: 'Warrior',
+    2514244534: 'Bile Titan',
+    2664856027: 'Shredder Tank',
     4211847317: 'Voteles',
-    1405979473: 'Harvester'
 }
 
 def get_enemy(id:int) -> str:
     if id in enemy_units:
         return enemy_units[id]
     else:
+        print(f"unknown unit id: {id}")
         return None
