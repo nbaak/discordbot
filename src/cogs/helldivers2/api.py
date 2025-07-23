dafa90970adf66e1b57be8f3b80f0d7064a4b67d
nbaak <niklas@baak.xyz>
2025-07-23 08:38:35 +0200

    added parameters for get request and cleaned up ap ia little

--- src/cogs/helldivers2/api.py
+++ src/cogs/helldivers2/api.py
@@ -8,12 +8,12 @@ class Endpoints:
     all = "https://api.diveharder.com/v1/all"
     war_statistics = "https://api.helldivers2.dev/api/v1/war"
     # dispatch = "https://api.helldivers2.dev/a/pi/v1/dispatches?maxEntries=1024"
-    dispatch = "https://api.live.prod.thehelldiversgame.com/api/NewsFeed/801?maxEntries=1024"
+    dispatch = "https://api.live.prod.thehelldiversgame.com/api/NewsFeed/801"
     campaign = "https://api.helldivers2.dev/api/v1/campaigns"
     planets = "https://api.helldivers2.dev/api/v1/planets"
     major_order = "https://api.helldivers2.dev/api/v1/assignments"
-    
-    
+
+
 def get_all():
     headers = {"X-Super-Client": X_SUPER_CLIENT, "X-Super-Contact": X_SUPER_CONTACT}
     return httpclient.get(Endpoints.all, headers)
@@ -45,15 +45,20 @@ def planets():
 
 
 def dispatch():
-    headers = {"X-Super-Client": X_SUPER_CLIENT, "X-Super-Contact": X_SUPER_CONTACT, "Accept-Language": "de,en-US;q=0.7,en;q=0.3",}
-    return httpclient.get(Endpoints.dispatch, headers)
+    headers = {"X-Super-Client": X_SUPER_CLIENT,
+               "X-Super-Contact": X_SUPER_CONTACT,
+               "Accept-Language": "de,en-US;",
+               }
+    parameters = {"maxEntries": 1024}
+    return httpclient.get(Endpoints.dispatch, headers, parameters)
 
 
 def test():
     print(get_major_order())
     print(planets()[126])
     print(get_campaign())
-    
+    print(dispatch()[-1])
+
 
 if __name__ == "__main__":
     test()
--- src/cogs/helldivers2/httpclient.py
+++ src/cogs/helldivers2/httpclient.py
@@ -2,8 +2,8 @@ import requests
 from typing import Union
 
 
-def get(endpoint:str, headers:Union[dict, None]=None):
+def get(endpoint:str, headers:Union[dict, None]=None, params:Union[dict, None]=None) -> Union[dict, None]:
     headers = headers or {}
-    response = requests.get(endpoint, headers=headers)
+    response = requests.get(endpoint, headers=headers, params=params)
     
     return response.json()
